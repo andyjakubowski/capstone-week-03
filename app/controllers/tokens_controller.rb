4c69122262d7349dd245b227159f0925330bdeba
Andy Jakubowski <hello@andyjakubowski.com>
2020-02-24 15:18:25 +0100

    Set up JSON templates for Token routes

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,66 +1,72 @@
 class TokensController < ApplicationController
   before_action :set_list
 
-  # GET /tokens
-  # GET /tokens.json
+  # GET /lists/5/tokens
+  # GET /lists/5/tokens.json
   def index
     @tokens = @list.tokens
   end
 
-  # GET /tokens/1
-  # GET /tokens/1.json
+  # GET /lists/5/tokens/1
+  # GET /lists/5/tokens/1.json
   def show
     @token = Token.find(params[:id])
     # @list = List.find(params[:list_id])
   end
 
-  # GET /tokens/new
+  # GET lists/5/tokens/new
   def new
     @token = Token.new
   end
 
-  # GET /tokens/1/edit
+  # GET /lists/5/tokens/1/edit
   def edit
     @token = Token.find(params[:id])
   end
 
-  # POST /tokens
-  # POST /tokens.json
+  # POST /lists/5/tokens
+  # POST /lists/5/tokens.json
   def create
     @token = @list.tokens.new(token_params)
-
     respond_to do |format|
       if @token.save
         format.html { redirect_to list_path(@list), notice: 'Token was successfully created.' }
-        # format.json { render :show, status: :created, location: @token }
+        format.json { render :show, status: :created, location: list_token_url(@list, @token) }
       else
         format.html { render 'lists/show' }
-        # format.json { render json: @token.errors, status: :unprocessable_entity }
+        format.json { render json: @token.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /tokens/1
-  # PATCH/PUT /tokens/1.json
+  # PATCH/PUT /lists/5/tokens/1
+  # PATCH/PUT /lists/5/tokens/1.json
   def update
-    @list = List.find(params[:list_id])
     @token = @list.tokens.find(params[:id])
 
-    if @token.update(token_params)
-      redirect_to @list
-      # format.html { redirect_to @list, notice: 'Token was successfully updated.' }
-    else
-      render 'edit'
+    respond_to do |format|
+      if @token.update(token_params)
+        # redirect_to @list
+        format.html { redirect_to @list, notice: 'Token was successfully updated.' }
+        format.json { render :show, status: :ok, location: list_token_url(@list, @token) }
+      else
+        format.html { render 'edit' }
+      end
     end
+
+    
   end
 
-  # DELETE /tokens/1
-  # DELETE /tokens/1.json
+  # DELETE /lists/5/tokens/1
+  # DELETE /lists/5/tokens/1.json
   def destroy
-    @list = List.find(params[:list_id])
     @token = @list.tokens.find(params[:id])
     @token.destroy
-    redirect_to list_path(@list)
+
+    respond_to do |format|
+      format.html { redirect_to @list }
+      format.json { head :no_content }
+    end
   end
 
   private
@@ -71,6 +77,6 @@ class TokensController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def token_params
-      params.require(:token).permit(:name, :value, :category_id)
+      params.require(:token).permit(:name, :value, :category_id, :list_id)
     end
 end
