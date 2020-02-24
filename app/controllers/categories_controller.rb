78714987645e81502d186fb6cc8272aba6f586e5
Andy Jakubowski <hello@andyjakubowski.com>
2020-02-24 16:30:36 +0100

    Set up JSON request handling for Categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,17 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+
+    respond_to do |format|
+      format.html { render 'other/404' }
+      format.json { render :index, status: :ok }
+    end
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
   def new
     @category = Category.new
   end
@@ -10,27 +23,38 @@ class CategoriesController < ApplicationController
   def create
     @category = Category.new(category_params)
 
-    if @category.save
-      redirect_to lists_path
-    else
-      format.html { render 'new' }
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to lists_path, notice: 'Category created.' }
+        format.json { render :show, status: :created, location: @category }
+      else
+        format.html { render 'new' }
+      end
     end
   end
 
   def update
     @category = Category.find(params[:id])
-    if @category.update(category_params)
-      notice = 'Successfully edited the category.'
-      redirect_to lists_path
-    else
-      render 'edit'
+
+    respond_to do |format|
+      if @category.update(category_params)
+        format.html { redirect_to lists_path, notice: 'Successfully edited the category.' }
+        format.json { render :show, status: :ok, location: @category }
+      else
+        format.html { render 'edit' }
+      end
+
     end
   end
 
   def destroy
     @category = Category.find(params[:id])
     @category.destroy
-    redirect_to lists_url
+
+    respond_to do |format|
+      format.html { redirect_to lists_url, notice: 'Category deleted.' }
+      format.json { head :no_content }
+    end
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_094433) do
+ActiveRecord::Schema.define(version: 2020_02_24_144203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["name"], name: "index_categories_on_name", unique: true
